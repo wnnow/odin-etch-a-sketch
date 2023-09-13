2e64004927e9fcbfd3de486ed7452fc5f3ad85fa
wnnow <117201813+wnnow@users.noreply.github.com>
2023-09-14 06:25:53 +0700

    Rework box size use js instead of CSS hardcode px

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-console.log("Hello World");
+const container = document.querySelector(".container");
 
 function createBox() {
   const container = document.querySelector(".container");
   const div = document.createElement("div");
   div.classList.add("box");
+  div.style.width = `${square - 2}px`;
+  div.style.height = `${square - 2}px`;
   container.appendChild(div);
 }
-
-for (let i = 0; i < 625; i++) {
+let square = 16;
+container.style.height = `${square * square}px`;
+container.style.width = `${square * square}px`;
+for (let i = 0; i < square * square; i++) {
   createBox();
 }
+
+const boxs = document.querySelectorAll(".box");
+boxs.forEach((box) =>
+  box.addEventListener("mouseover", (e) => {
+    console.log(e);
+    e.target.style.backgroundColor = "black";
+  })
+);
