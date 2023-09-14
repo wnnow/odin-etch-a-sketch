ec49b2985fd92d6e7c38465d3dd2bec8ff78c9bc
wnnow <117201813+wnnow@users.noreply.github.com>
2023-09-14 07:06:21 +0700

    Add user input, Rework calculate box code, Fix container
    Add user input via prompt, receive user value then calculate box size
    Box size will scale depend to user input
    Fix container width and height

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
 const container = document.querySelector(".container");
 
+const containerHeight = container.clientHeight;
+const containerWidth = container.clientWidth;
+let squareNumber;
+
 function createBox() {
   const container = document.querySelector(".container");
   const div = document.createElement("div");
   div.classList.add("box");
-  div.style.width = `${square - 2}px`;
-  div.style.height = `${square - 2}px`;
+  div.style.width = `${containerHeight / squareNumber - 2}px`;
+  div.style.height = `${containerWidth / squareNumber - 2}px`;
   container.appendChild(div);
 }
-let square = 16;
-container.style.height = `${square * square}px`;
-container.style.width = `${square * square}px`;
-for (let i = 0; i < square * square; i++) {
-  createBox();
+
+function createBoxLoop(squareNumber) {
+  for (let i = 0; i < squareNumber * squareNumber; i++) {
+    createBox();
+  }
 }
 
 const boxs = document.querySelectorAll(".box");
 boxs.forEach((box) =>
   box.addEventListener("mouseover", (e) => {
-    console.log(e);
     e.target.style.backgroundColor = "black";
   })
 );
+
+const btn = document.querySelector("#create-square-btn");
+btn.addEventListener("click", () => {
+  squareNumber = +prompt("How many squre do you want? (Max 100)", 100);
+  console.log(squareNumber);
+  createBoxLoop(squareNumber);
+  return squareNumber;
+});
